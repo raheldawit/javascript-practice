67a3fb10af9967716b48e6890746c83b23635c1c
rahel <raheldawit21@yahoo.com>
2022-02-27 21:18:41 -0600

    update

--- js/script.js
+++ js/script.js
@@ -2,4 +2,8 @@
 /*Homework*/
 /*02/26/2022*/
 
-console.log(`Rahel`)
+console.log(`My name Rahel`);
+console.log(`My favorite food injera`);
+// Daily Challenge        
+/* Day 1  Javascript for Beginners
+Challenge.*/  
